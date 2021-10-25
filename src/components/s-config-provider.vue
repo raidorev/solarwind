3d08a304ab65b10421c78771b5ab198039c0d509
Alexander Titov <fox@raidorev.tech>
2021-10-25 23:12:45 +0600

    feat(config-provider): config shape validation

--- src/components/s-config-provider.vue
+++ src/components/s-config-provider.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { provide, inject } from 'vue'
+import * as s from 'superstruct'
 import { defaultsDeep } from 'lodash-es'
 import { configSymbol } from 'solarwind/utils/symbols'
 import type { PartialDeep } from 'type-fest'
@@ -10,9 +11,26 @@ type Props = {
 }
 const props = defineProps<Props>()
 
-const oldConfig = inject(configSymbol, {})
+// If there is no arguments, vue logs warning when injection not found
+// eslint-disable-next-line unicorn/no-useless-undefined
+const oldConfig = inject(configSymbol, undefined)
 
-provide(configSymbol, defaultsDeep(props.config, oldConfig))
+// inject can't be used outside setup function (e.g. prop validator),
+// this is why validation happens here, not in prop validator
+const configSchema:
+  | s.Describe<SolarwindConfig>
+  | s.Describe<PartialDeep<SolarwindConfig>> = s.object({})
+
+const [error, config] = configSchema.validate(props.config)
+
+if (error) {
+  console.warn(
+    'Invalid configuration provided inside "s-config-provider" component',
+  )
+  throw error
+}
+
+provide(configSymbol, defaultsDeep(config, oldConfig))
 </script>
 
 <template>
