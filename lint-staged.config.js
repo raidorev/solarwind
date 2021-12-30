f9325fbc324aa41892682dee3d52613a5de66afc
Alexander Titov <fox@raidorev.tech>
2021-12-30 22:59:18 +0600

    chore: change lint-staged configuration

---
+++ lint-staged.config.js
@@ -0,0 +1,18 @@
+const withFiles = (commands) => (filenames) =>
+  // eslint-disable-next-line lodash/prefer-lodash-method
+  commands.map((command) => `${command} ${filenames.join(' ')}`)
+
+module.exports = {
+  '*.{js,jsx,ts,tsx,vue,md,css,sass,scss,html}': withFiles([
+    `prettier --write`,
+  ]),
+  '*.(js|jsx|ts|tsx|vue)': withFiles([
+    'eslint --cache',
+    'jest --bail --findRelatedTests',
+  ]),
+  'yarn.lock': () => [
+    'yarn dedupe',
+    'yarn dedupe --check',
+    'yarn dlx @yarnpkg/doctor',
+  ],
+}
