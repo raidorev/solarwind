a97413c5d13b28a8306e6f0e52124def636b453b
Alexander Titov <fox@raidorev.tech>
2022-01-03 20:10:29 +0600

    chore: add framework colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`
+  }
+}
+
 module.exports = {
   purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: withOpacityValue('--s-color-background'),
+        onBackground: withOpacityValue('--s-color-onBackground'),
+        surface: withOpacityValue('--s-color-surface'),
+        onSurface: withOpacityValue('--s-color-onSurface'),
+        primary: withOpacityValue('--s-color-primary'),
+        onPrimary: withOpacityValue('--s-color-onPrimary'),
+        secondary: withOpacityValue('--s-color-secondary'),
+        onSecondary: withOpacityValue('--s-color-onSecondary'),
+        error: withOpacityValue('--s-color-error'),
+        onError: withOpacityValue('--s-color-onError'),
+        warning: withOpacityValue('--s-color-warning'),
+        onWarning: withOpacityValue('--s-color-onWarning'),
+        success: withOpacityValue('--s-color-success'),
+        onSuccess: withOpacityValue('--s-color-onSuccess'),
+      },
+    },
   },
   variants: {
     extend: {},
