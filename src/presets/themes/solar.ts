f93c039fb0ae9285a4ddd2aeb3d7e457ddbba7b2
Alexander Titov <fox@raidorev.tech>
2022-01-03 20:11:24 +0600

    chore(theme): use rgb channels in theme preset

--- src/presets/themes/solar.ts
+++ src/presets/themes/solar.ts
@@ -1,21 +1,27 @@
-import { type SolarwindTheme } from 'solarwind'
+import { type SolarwindTheme } from 'solarwind/types'
+import { rgbaChannelsFromHex } from 'solarwind/utils/color'
+
+const rgb = (hex: string) => {
+  const [r, g, b] = rgbaChannelsFromHex(hex)
+  return `${r} ${g} ${b}`
+}
 
 export const solar: SolarwindTheme = {
   name: 'solar',
   colors: {
-    background: '#121212',
-    onBackground: '#ffffff',
-    surface: '#121212',
-    onSurface: '#121212',
-    primary: '#d95962',
-    onPrimary: '#000000',
-    secondary: '#713ba3',
-    onSecondary: '#000000',
-    error: '#b00020',
-    onError: '#ffffff',
-    warning: '#bbbb00',
-    onWarning: '#ffffff',
-    success: '#23bbbb',
-    onSuccess: '#ffffff',
+    background: rgb('#121212'),
+    onBackground: rgb('#ffffff'),
+    surface: rgb('#121212'),
+    onSurface: rgb('#121212'),
+    primary: rgb('#d95962'),
+    onPrimary: rgb('#000000'),
+    secondary: rgb('#713ba3'),
+    onSecondary: rgb('#000000'),
+    error: rgb('#b00020'),
+    onError: rgb('#ffffff'),
+    warning: rgb('#bbbb00'),
+    onWarning: rgb('#ffffff'),
+    success: rgb('#23bbbb'),
+    onSuccess: rgb('#ffffff'),
   },
 }
